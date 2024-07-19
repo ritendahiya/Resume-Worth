f879786ef319c966109f4d3a0ef5bdff25bd43a5
ritendahiya <142141229+ritendahiya@users.noreply.github.com>
2024-07-19 15:13:51 +0530

    initial

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import Image from "next/image";
+import ResumeAnalyzerApp from "./components/ResumeAnalyzerApp";
+import styles from "./styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.App}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.centeredContent}>
+            <div className={styles.logoBox}>
+              <Image
+                src="/logo.png"
+                alt="InterviewGPT logo"
+                width="400"
+                height="75"
+              />
+            </div>
+            <ResumeAnalyzerApp />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
